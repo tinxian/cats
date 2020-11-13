0ae16d762d23b19ba82269b5a56589221435724f
Diaq <imcrazydia@gmail.com>
2020-11-13 17:15:25 +0100

    fix, whole API
    Samen in een call gezeten met het fixen van onze API

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,109 @@
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:http/http.dart' as http;
-
-Future<List<Photo>> fetchPhotos(http.Client client) async {
-  final response = await client.get('https://tinxian.nl/cats.json');
-
-  // Use the compute function to run parsePhotos in a separate isolate.
-  return compute(parsePhotos, response.body);
-}
-
-// A function that converts a response body into a List<Photo>.
-List<Photo> parsePhotos(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-
-  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
-}
-
-class Photo {
-  final String name;
-  final String date;
-  final String thumbnailUrl;
-
-  Photo({this.name, this.date, this.thumbnailUrl});
-
-  factory Photo.fromJson(Map<String, dynamic> json) {
-    return Photo(
-      name: json['name'] as String,
-      date: json['dob'] as String,
-      thumbnailUrl: json['image'] as String,
-    );
-  }
-}
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Cats Demo';
-
     return MaterialApp(
-      title: appTitle,
-      home: MyHomePage(title: appTitle),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Api Call'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
   final String title;
 
-  MyHomePage({Key key, this.title}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull("http://hers.hosts1.ma-cloud.nl/catabase/getcats.php"),
+        headers: {"Accept": "application/json"});
+
+    setState(() {
+      data = json.decode(response.body)['cats'];
+    });
+    return "Success";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("Flutter Cats API"),
       ),
-      body: FutureBuilder<List<Photo>>(
-        future: fetchPhotos(http.Client()),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          return snapshot.hasData
-              ? PhotosList(photos: snapshot.data)
-              : Center(child: CircularProgressIndicator());
-        },
+      body: Center(
+        child: getList(),
       ),
     );
   }
-}
 
-class PhotosList extends StatelessWidget {
-  final List<Photo> photos;
-
-  PhotosList({Key key, this.photos}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      itemCount: photos.length,
-      itemBuilder: (context, index) {
-        return Image.network(photos[index].thumbnailUrl);
+  Widget getList() {
+    if (data == null || data.length < 1) {
+      return Container(
+        child: Center(
+          child: Text("Please wait..."),
+        ),
+      );
+    }
+    return ListView.separated(
+      itemCount: data?.length,
+      itemBuilder: (BuildContext context, int index) {
+        return getListItem(index);
       },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    );
+  }
+
+  Widget getListItem(int i) {
+    if (data == null || data.length < 1) return null;
+    if (i == 0) {
+      return Container(
+        margin: EdgeInsets.all(4),
+        child: Center(
+          child: Text(
+            "Cats",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(data[i]['name'].toString()),
+          Text(data[i]['dob'].toString()),
+          Image.network(data[i]['image'].toString()),
+        ],
+      ),
     );
   }
 }
